d9db7c74d8b733940b9d8fee7cc765fc8878f315
hakima <hakima.chapron@epitech.eu>
2019-01-21 17:13:17 +0100

    index8

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8"><!--Encodage latin de la page -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Site de rencontre"/>
+		<link rel="icon" href="./Images/logo.png"/>
+		<link rel="stylesheet" type="text/css" href="./css/styles.css">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap-grid.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap-reboot.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+		<!-- CDN -->
+		<!-- Bootstrap CSS --> 
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href=".css/cinema.css">
+		<!-- js scipts-->
+		<script src="./js/script.js"></script>
+		<title>Tweet academie</title>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="navbar-brand">
+					<img src="./Images/logo.png" style="width: 40px; height: 40px;">
+				</div>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search Tweet academie">
+					<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Tweet academie</button>
+				</form>
+			</nav>
+		</header>
