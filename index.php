b9b341bc2a65477d604964344d0004069b0cf42f
hakima <hakima.chapron@epitech.eu>
2019-01-21 13:53:41 +0100

    index

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8"><!--Encodage latin de la page -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Site de rencontre"/>
+		<link rel="icon" href="./Images/logo.png"/>
+		<link rel="stylesheet" type="text/css" href="./css/styles.css">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap-grid.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap-reboot.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+		<!-- CDN -->
+		<!-- Bootstrap CSS --> 
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href=".css/cinema.css">
+		<!-- js scipts-->
+		<script src="./js/script.js"></script>
+		<title>Tweet academie</title>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="navbar-brand">
+					<img src="./Images/logo.png" style="width: 40px; height: 40px;">
+					<a class="btn btn-primary" href="" role="button">Register</a>
+					<a class="btn btn-primary" href="">Nous contacter</a>
+				</div>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Chercher">
+					<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Chercher</button>
+				</form>
+			</nav>
+		</header>
+		<div class="container">
+			<div id="login-row" class="row justify-content-center align-items-center">
+				<div id="login-column" class="col-md-6">
+					<div class="box">
+						<div class="float">
+							<form class="form" action="">
+								<div class="form-group">
+									<label for="username" class="text-white">Username:</label><br>
+									<input type="text" name="username" id="username" class="form-control">
+								</div>
+								<div class="form-group">
+									<label for="password" class="text-white">Password:</label><br>
+									<input type="text" name="password" id="password" class="form-control">
+								</div>
+								<div class="form-group">
+									<input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html> 	
