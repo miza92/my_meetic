419cc3a4f33f348af28defbe6fad097144e2fe9d
hakima <hakima.chapron@epitech.eu>
2019-01-22 10:06:42 +0100

    index9

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8"><!--Encodage latin de la page -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="Site de rencontre"/>
+		<meta name="description" content="Réseau social"/>
 		<link rel="icon" href="./img/logo.png"/>
 		<link rel="stylesheet" type="text/css" href="./css/styles.css">
 		<link rel="stylesheet" type="text/css" href="./css/bootstrap-grid.min.css">
@@ -17,8 +17,22 @@
 		<title>Tweet academie</title>
 	</head>
 	<body class="body">
+		<body class="body">
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="navbar-brand">
+					<img src="./img/logo.png" style="width: 40px; height: 40px;">
+					<a class="btn btn-primary" href="register.php" role="button">Login</a>
+					<a class="btn btn-primary" href="index.php">Home</a>
+				</div>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="search">
+					<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">search</button>
+				</form>
+			</nav>
+		</header>
 		<div class="container">
-			<form class="form-horizontal" role="form" method="POST" action="./index.php?page=inscription">
+			<form class="form-horizontal" role="form" method="POST" action="login.php">
 				<div class="row">
 					<div class="col-md-3"></div>
 					<div class="col-md-6">
